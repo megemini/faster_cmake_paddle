1614598aaab954334304571b42cdbf6577aa8aed
megemini <megemini@outlook.com>
2023-10-20 12:36:09 +0800

    [Init] add faster.sh

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # faster_cmake_paddle
-faster cmake paddle
+通过脚本将 `cmake` 需要拉取的 `github` 地址替换为 `gitclone`。
+
+### Step 1
+
+将 `faster.sh` 拷贝到 `/Paddle/build` 目录下，也就是编译需要的文件夹。
+
+### Step 2
+
+赋予 `faster.sh` 可执行权限
+
+``` shell
+$> chmod +x faster.sh
+```
+
+### Step 3
+
+通过 `faster.sh` 执行 `cmake` 命令，如：
+
+``` shell
+./faster.sh time cmake .. -DPY_VERSION=3.8 -DWITH_GPU=OFF -DWITH_TESTING=ON
+```
+
+脚本会先 `patch` `github` 地址，然后执行后面的 `cmake` 命令，命令执行完之后再 `restore` 修改的文件。
+
+也可以单独 `patch` `restore`，如：
+
+``` shell
+$> ./faster.sh patch
+$> ./faster.sh restore
+```
+
+
+
---
+++ faster.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+function patch(){
+  echo 'patch ../CMakeLists.txt ../.gitmodules'
+  sed -i "s/https\:\/\/github.com/https\:\/\/gitclone.com\/github.com/g" ../CMakeLists.txt ../.gitmodules
+}
+
+function restore(){
+  echo 'restore ../CMakeLists.txt ../.gitmodules'
+  sed -i "s/https\:\/\/gitclone.com\/github.com/https\:\/\/github.com/g" ../CMakeLists.txt ../.gitmodules
+}
+
+if [[ $1 == patch ]]; then
+  patch
+
+elif [[ $1 == restore ]]; then
+  restore
+
+elif [[ $1 == cmake || ( $1 == time && $2 == cmake ) ]]; then
+  patch
+  eval $*
+  restore
+
+else
+  echo "Please use: patch, restore or cmake."
+
+fi
