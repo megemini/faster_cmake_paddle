3be2f050ccc9db4357ed266720c92053413cc05b
megemini <megemini@outlook.com>
2023-10-21 14:51:59 +0800

    [Change] use perl

--- faster.sh
+++ faster.sh
@@ -1,36 +1,40 @@
 #!/bin/bash
 
+GIT_RAW_URL='https\:\/\/github\.com'
+GIT_CLONE_URL='https\:\/\/gitclone\.com\/github\.com'
+
+GIT_URL_VAR='\$\{GIT_URL\}'
+GIT_PACKAGE_URL=${GIT_URL_VAR}'(.*(archive|download))'
+GIT_DOWN_URL_0='https\:\/\/gh\.api\.99988866\.xyz\/https\:\/\/github\.com'
+GIT_DOWN_URL_1=${GIT_DOWN_URL_0}'(.*(archive|download))'
+
+
 function patch(){
   echo 'patch: ../CMakeLists.txt ../.gitmodules'
-  sed -i "s/https\:\/\/github\.com/https\:\/\/gitclone\.com\/github\.com/g" ../CMakeLists.txt ../.gitmodules
+  sed -i "s/"${GIT_RAW_URL}"/"${GIT_CLONE_URL}"/g" ../CMakeLists.txt ../.gitmodules
 
   cd ../cmake/external
   dir=$( ls -l ./ | awk '!/^d/ {print $NF}' | grep -i cmake$ )
   for i in $dir
   do
     echo 'patch: ../cmake/external/'$i
-
-    sed -i 's/"${GIT_URL}\(.*archive.*\)"/"https\:\/\/gh\.api\.99988866\.xyz\/https\:\/\/github\.com\1"/g' $i
-    sed -i 's/"${GIT_URL}\(.*download.*\)"/"https\:\/\/gh\.api\.99988866\.xyz\/https\:\/\/github\.com\1"/g' $i
-
+    perl -pi -e "s/"${GIT_PACKAGE_URL}"/"${GIT_DOWN_URL_0}"\1/g" $i
   done
-
+  cd ../../build
 }
 
 function restore(){
   echo 'restore ../CMakeLists.txt ../.gitmodules'
-  sed -i "s/https\:\/\/gitclone\.com\/github\.com/https\:\/\/github\.com/g" ../CMakeLists.txt ../.gitmodules
+  sed -i "s/"${GIT_CLONE_URL}"/"${GIT_RAW_URL}"/g" ../CMakeLists.txt ../.gitmodules
 
   cd ../cmake/external
   dir=$( ls -l ./ | awk '!/^d/ {print $NF}' | grep -i cmake$ )
   for i in $dir
   do
     echo 'restore: ../cmake/external/'$i
-
-    sed -i 's/"https\:\/\/gh\.api\.99988866\.xyz\/https\:\/\/github\.com\(.*\)"/"${GIT_URL}\1"/g' $i
-
+    perl -pi -e "s/"${GIT_DOWN_URL_1}"/"${GIT_URL_VAR}"\1/g" $i
   done
-
+  cd ../../build
 }
 
 if [[ $1 == patch ]]; then
